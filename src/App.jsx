77f445ca27cf946ddfb236be096b490feab3877e
jeangathoni8 <jeangathoni39@gmail.com>
2024-05-08 18:12:45 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { v4 as uuidv4 } from "uuid";
+import NavBar from "./Components/NavBar/NavBar";
+import News from "./components/News/News";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { router } from "./Components/Configuration/Config";
+import Search from "./Components/Search/SearchBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Router>
+        <NavBar />
+        <Routes>
+          {router.map((path) => (
+            <Route
+              exact
+              key={uuidv4()}
+              path={path.path}
+              element={
+                <News
+                  key={path.key}
+                  newscategory={path.category}
+                  country={path.country}
+                />
+              }
+            />
+          ))}
+          <Route path="/search/:query" element={<Search />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
